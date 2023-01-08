725569f9add04127b2b0ddd0629b4f95ea82241c
sungho.byun <sungho.byun@lgepartner.com>
2023-01-08 12:55:49 +0900

    4.2 User Layer 구현

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+
+        try {
+            //요청을 이용해 저장할 사용자 만들기
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+
+            //서비스를 이용해 리포지터리에 사용자 저장
+            UserEntity registerdUser = userService.create(user);
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(registerdUser.getEmail())
+                    .username(registerdUser.getUsername())
+                    .id(registerdUser.getId())
+                    .build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            //사용자 정보는 하나이기 때문에 리스트로 만들 필요 없이 UserDTO 바로 리턴
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity
+                    .badRequest()
+                    .body(responseDTO);
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO){
+        UserEntity user = userService.getByCredentials(
+                userDTO.getEmail(), userDTO.getPassword()
+        );
+
+        if (user != null) {
+            final UserDTO responseUserDTO = UserDTO.builder()
+                    .email(user.getEmail())
+                    .id(user.getId())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        }else{
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login failed.")
+                    .build();
+
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+}
